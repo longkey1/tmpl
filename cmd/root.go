d32e7b8bc1431189244bc41ab4a215218df3b330
longkey1 <longkey1@gmail.com>
2022-04-07 12:22:23 +0900

    build: v0.3.4

--- cmd/root.go
+++ cmd/root.go
@@ -34,7 +34,7 @@ var config Config
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Version: "0.3.3",
+	Version: "0.3.4",
 	Use:   "tmpl",
 	Short: "template file linker",
 	// Uncomment the following line if your bare application
